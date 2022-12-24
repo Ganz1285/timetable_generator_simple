e810d6a51a08d1b480fa50fb07c4dfd3eaa65bd8
selva <selva.g@subcom.tech>
2022-12-24 12:19:34 +0530

    "added a rndom function to shuffle classes"

--- y.py
+++ y.py
@@ -1,133 +1,239 @@
+"""
+    TIMETABLE GENERATOR SIMPLE
+
+"""
+
+__author__ = "Selvaganapathy K"
+__email__ = "selvaganz1285@gmail.com"
+
+
 import numpy as np
 from numpy import random
 
 
 class subject:
-    def __init__(self,id,name,instructor,hours,year,islab):
-        self.id=id
-        self.name=name
-        self.instructor=instructor
-        self.hours=hours
-        self.year=year
-        self.islab=islab
+    """
+    details of each subjects
 
+    """
 
+    def __init__(self, id, name, instructor, hours, year, islab):
+        self.id = id
+        self.name = name
+        self.instructor = instructor
+        self.hours = hours
+        self.year = year
+        self.islab = islab
 
 
 class Class:
-    def __init__(self,day,year,hour):
-        self.day=day
-        self.year=year
-        self.hour=hour
-        self.subject=None
+    """
+    details of each class in year and day-order
 
+    """
 
+    def __init__(self, day, year, hour):
+        self.day = day
+        self.year = year
+        self.hour = hour
+        self.subject = None
 
 
 class years:
-    def __init__(self,year):
-        self.year=year
-        self.subjects=[]
-        self.instructors=[]
-        self.lab=None
+    """
+    details of each year of a programme
 
+    """
 
+    def __init__(self, year):
+        self.year = year
+        self.subjects = []
+        self.instructors = []
+        self.lab = None
 
 
 def define_each_classes():
-    each_classes={}
-    num_dayorders=6
-    num_years=5
-    num_hoursperday=5
-    for i in range(1,num_years+1):
-        each_classes[i]=[]
-        for j in range(1,num_dayorders+1):
+    each_classes = {}
+    num_dayorders = 6
+    num_years = 5
+    num_hoursperday = 5
+    for i in range(1, num_years + 1):
+        each_classes[i] = []
+        for j in range(1, num_dayorders + 1):
             each_classes[i].append([])
-            for k in range(1,num_hoursperday+1):
-                each_classes[i][j-1].append(Class(j,i,k))
-
+            for k in range(1, num_hoursperday + 1):
+                each_classes[i][j - 1].append(Class(j, i, k))
 
     return each_classes
 
 
 def define_subjects():
-    l={}
-    subjects={1:['DP-2:Developmental Psychology – II:NAC:5', 'GP-2:General Psychology – II:RUK:5', 'PS-1:Psychological Statistics - I:SRS:5', 'TAM-1:TAMIL:LANG-1:6', 'ENG-1:ENGLISH:LANG-2:6', 'LAB:EP-1:Experimental Psychology- Practical-I :VAO:3'],2: ['AP-1:Abnormal Psychology-I:KRT:5', 'RM:Research Methodology:NAC:5', 'LAP:Legal Aspects of Psychology:SRS:5', 'TAM-2:TAMIL:LANG-1:5', 'LANG-2:ENGLISH:LANG-2:5', 'LAB:EP-3:Experimental Psychology- Practical-III:KAN:3', 'EDC:IS:Information Security:EDC:2'],3: ['FMCS:Fundamentals of Marketing and Consumer Behavior:RUK:5', 'FC:Fundamentals of Counselling:ANU:5', 'FHP:Fundamentals of Health Psychology:KAA:5', 'PEC:Psychology of Exceptional Children:KAN:5', 'RP:Rehabilitation Psychology:VAO:5', 'LAB:EP-5:Experimental Psychology- Practical – V- Case Analysis:KRT:5'],4: ['OB:Organizational Behavior:ANU:5', 'CP:Clinical Psychology:RUK:5', 'PTD:Psychological Testing & Diagnosis:KAA:5', 'BM:Behavior Modification:VAO:5', 'LAB:PA:Psychological Assessment- Practical II:SRS:5', 'EDC:CS:Cyber Security:EDC:5'],5: ['OB-2:Organizational Behavior - II:NAC:5', 'MCB:Marketing & Consumer Behavior:KAN:5', 'CS:Counseling Psychology:KRT:5', 'HP:Health Psychology:ANU:4', 'LAB:CA:Case Analysis:KAA:3', 'EDC:PS:Professional Skills:EDC:4', 'EDC:HRM:Human Resource Management:EDC:4']}
+    l = {}
+    subjects = {
+        1: [
+            "DP-2:Developmental Psychology – II:NAC:5",
+            "GP-2:General Psychology – II:RUK:5",
+            "PS-1:Psychological Statistics - I:SRS:5",
+            "TAM-1:TAMIL:LANG-1:6",
+            "ENG-1:ENGLISH:LANG-2:6",
+            "LAB:EP-1:Experimental Psychology- Practical-I :VAO:3",
+        ],
+        2: [
+            "AP-1:Abnormal Psychology-I:KRT:5",
+            "RM:Research Methodology:NAC:5",
+            "LAP:Legal Aspects of Psychology:SRS:5",
+            "TAM-2:TAMIL:LANG-1:5",
+            "LANG-2:ENGLISH:LANG-2:5",
+            "LAB:EP-3:Experimental Psychology- Practical-III:KAN:3",
+            "EDC:IS:Information Security:EDC:2",
+        ],
+        3: [
+            "FMCS:Fundamentals of Marketing and Consumer Behavior:RUK:5",
+            "FC:Fundamentals of Counselling:ANU:5",
+            "FHP:Fundamentals of Health Psychology:KAA:5",
+            "PEC:Psychology of Exceptional Children:KAN:5",
+            "RP:Rehabilitation Psychology:VAO:5",
+            "LAB:EP-5:Experimental Psychology- Practical – V- Case Analysis:KRT:5",
+        ],
+        4: [
+            "OB:Organizational Behavior:ANU:5",
+            "CP:Clinical Psychology:RUK:5",
+            "PTD:Psychological Testing & Diagnosis:KAA:5",
+            "BM:Behavior Modification:VAO:5",
+            "LAB:PA:Psychological Assessment- Practical II:SRS:5",
+            "EDC:CS:Cyber Security:EDC:5",
+        ],
+        5: [
+            "OB-2:Organizational Behavior - II:NAC:5",
+            "MCB:Marketing & Consumer Behavior:KAN:5",
+            "CS:Counseling Psychology:KRT:5",
+            "HP:Health Psychology:ANU:4",
+            "LAB:CA:Case Analysis:KAA:3",
+            "EDC:PS:Professional Skills:EDC:4",
+            "EDC:HRM:Human Resource Management:EDC:4",
+        ],
+    }
     for j in subjects:
-        l[j]=[]
+        l[j] = []
         for k in subjects[j]:
-            z=k.split(':')
-            if(len(z)==4):
-                l[j].append(subject(z[0],z[1],z[2],z[3],j,False))
+            z = k.split(":")
+            if len(z) == 4:
+                l[j].append(subject(z[0], z[1], z[2], z[3], j, False))
             else:
-                l[j].append(subject(z[1],z[2],z[3],z[-1],j,z[0]=="LAB"))
+                l[j].append(subject(z[1], z[2], z[3], z[-1], j, z[0] == "LAB"))
     return l
 
 
 def details_of_each_year(subject_details):
-    year=[]
+    year = []
     for j in subject_details:
-        x=years(j)
+        x = years(j)
         for k in subject_details[j]:
-            if k.year==j:
+            if k.year == j:
                 x.subjects.append(k.name)
                 x.instructors.append(k.instructor)
                 if k.islab:
-                    x.lab=k
-        
+                    x.lab = k
+
         year.append(x)
     return year
 
 
-    
-def schedule_labs(each_year,each_class):
+def schedule_labs(each_year, each_class):
     for i in each_year:
-        x=3
-        num_hours=int(i.lab.hours)
-        z=i.year
-        q=0
-        while num_hours!=1:
-            if num_hours>3:
-                if i.year==x:
-                    y=x+2
-                    each_class[i.year][y][z].subject=i.lab 
-                    z+=1
+        x = 3
+        num_hours = int(i.lab.hours)
+        z = i.year
+        q = 0
+        while num_hours != 1:
+            if num_hours > 3:
+                if i.year == x:
+                    y = x + 2
+                    each_class[i.year][y][z].subject = i.lab
+                    z += 1
                 else:
-                    y=x-2
-                    each_class[i.year][y][q].subject=i.lab
-                    q+=1
-                
-            else:
-                fetch=each_class[i.year][i.year-1][x].subject
-                if fetch is None:
-                    each_class[i.year][i.year-1][x].subject=i.lab
-                    x+=1
-            num_hours-=1
-        each_class[i.year][(i.year+1)%6][1].subject=i.lab
-        
+                    y = x - 2
+                    each_class[i.year][y][q].subject = i.lab
+                    q += 1
 
+            else:
+                fetch = each_class[i.year][i.year - 1][x].subject
+                if fetch is None:
+                    each_class[i.year][i.year - 1][x].subject = i.lab
+                    x += 1
+            num_hours -= 1
+        each_class[i.year][(i.year + 1) % 6][1].subject = i.lab
 
 
 def display_classes(each_class):
-    for i,t in each_class.items():
-        print("YEAR:",i)
-        print(" ".join(['{:5}'.format(q) for q in range(1,6)]))
-        z=1
+    for i, t in each_class.items():
+        print("YEAR:", i)
+        print(" ".join(["{:5}".format(q) for q in range(1, 6)]))
+        z = 1
         for k in t:
-            print(z,' '.join(['{:5}'.format(p.subject.id) if p.subject!=None else '{:5}'.format('None') for p in k ]))
-            z+=1
-            
+            print(
+                z,
+                " ".join(
+                    [
+                        "{:5}".format(p.subject.id)
+                        if p.subject != None
+                        else "{:5}".format("None")
+                        for p in k
+                    ]
+                ),
+            )
+            z += 1
+
         print()
+
+
+def shuffle_classes(each_subject, each_class):
+    d = {}
+    for i, t in each_subject.items():
+        d[i] = []
+        for k in t:
+            x = [k for _ in range(int(k.hours)) if not k.islab]
+            d[i].extend(x)
+        np.random.shuffle(d[i])
+    for z,q in d.items():
+        l=[]
+        for k in range(30):
+            x=k//5
+            y=k%5 
+            p=0
+            if k<len(q):
+                if each_class[z][x][y].subject is None:
+                    each_class[z][x][y].subject=d[z][k]
+                else:
+                    l.append(d[z][k])
+            else:
+                if each_class[z][x][y].subject is None:
+                    each_class[z][x][y].subject=l[p]
+                else:
+                    pass
+                p+=1
 
 
 
 
 if __name__ == "__main__":
-    each_class=define_each_classes()
-    each_subject=define_subjects()
-    each_year=details_of_each_year(each_subject)
-    schedule_labs(each_year,each_class)
+    each_class = define_each_classes()
+    # display_classes(each_class)
+    each_subject = define_subjects()
+    each_year = details_of_each_year(each_subject)
+    schedule_labs(each_year, each_class)
+
+    # shuffle_classes(each_subject, each_class)
     display_classes(each_class)
-    
+    # debug_result(each_class)
 
 
+
+
+
+"""
+INCORRECTLY WORKING FUNCTIONS:
+    1. shuffle_classes(each_subject, each_class):
+
+
+"""
